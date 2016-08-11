611a55ab6225ea5b6fbcff623a10b938e7c8650a
rhc <bawfnhaps@163.com>
2016-08-11 21:19:42 +0800

    InterpreterPattern

--- InterpreterPattern/main.m
+++ InterpreterPattern/main.m
@@ -7,11 +7,38 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Calculator.h"
+#import "Context.h"
+#import "RHCExpression.h"
+#import "Stack.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // 1
+        NSString *expStr = @"a + b - c";
+        NSDictionary *var = @{@"a":@"100",
+                              @"b":@"20",
+                              @"c":@"40"
+                              };
+        
+        Calculator *calculator = [[Calculator alloc]initWithExpStr:expStr];
+        
+        NSLog(@"运算结果为:%@=%ld",expStr,[calculator run:var]);
+        
+        
+        // 2
+        /*
+        Context *ctx = [Context new];
+        Stack *stack = [Stack new];
+        for (; ; ) {
+            // 进行语法判断，并产生递归调用
+        }
+        // 产生一个完整的语法树，由各个具体的语法分析进行解析
+        id<RHCExpression>exp = [stack pop];
+        // 具体元素进入场景
+        [exp interpreter:ctx];
+        */
+        
     }
     return 0;
 }
