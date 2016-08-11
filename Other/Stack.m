2bdbe9cea16edf3c4cb688cf6b3ce02453533056
rhc <bawfnhaps@163.com>
2016-08-11 21:35:35 +0800

    格式

--- Other/Stack.m
+++ Other/Stack.m
@@ -26,7 +26,7 @@
 
 @implementation Stack
 
- // 初始化
+// 初始化
 - (instancetype)init {
     if (self = [super init]) {
         _stackArray = [NSMutableArray new];
@@ -34,87 +34,87 @@
     return self;
 }
 
- // 入栈
- -(void)push:(id)objet {
+// 入栈
+-(void)push:(id)objet {
     [self.stackArray addObject:objet];
-   }
+}
 
- // 出栈
- -(id)pop {
-      id objc = [self.stackArray lastObject];
-      [self.stackArray removeLastObject];
-      return objc;
-  }
+// 出栈
+-(id)pop {
+    id objc = [self.stackArray lastObject];
+    [self.stackArray removeLastObject];
+    return objc;
+}
 
- // 返回栈顶元素
- -(id)TopElement {
-       return [self.stackArray lastObject];
-   }
+// 返回栈顶元素
+-(id)TopElement {
+    return [self.stackArray lastObject];
+}
 
- // 是否为空
- -(BOOL)isEmpty {
-       return self.stackArray.count;
-   }
+// 是否为空
+-(BOOL)isEmpty {
+    return self.stackArray.count;
+}
 
- // 栈的长度
- -(NSInteger)stackLength {
-       return self.stackArray.count;
-   }
+// 栈的长度
+-(NSInteger)stackLength {
+    return self.stackArray.count;
+}
 
- // 从底部开始遍历
- -(void)traversalElementFromBottom:(StackBlock)block {
-      NSEnumerator *objc = [self.stackArray objectEnumerator];
-   for (id element in objc) {
-           block(element);
-       }
-  }
+// 从底部开始遍历
+-(void)traversalElementFromBottom:(StackBlock)block {
+    NSEnumerator *objc = [self.stackArray objectEnumerator];
+    for (id element in objc) {
+        block(element);
+    }
+}
 
- // 从顶部开始遍历
- -(void)traversalElementFromtop:(StackBlock)block {
+// 从顶部开始遍历
+-(void)traversalElementFromtop:(StackBlock)block {
     // 先获取存储元素的个数
-      NSInteger count = self.stackArray.count;
-      for (NSInteger i = count; i > 0; i --) {
-          // 处理最后一个元素
-          block([self.stackArray objectAtIndex:i]);
-      }
-  }
+    NSInteger count = self.stackArray.count;
+    for (NSInteger i = count; i > 0; i --) {
+        // 处理最后一个元素
+        block([self.stackArray objectAtIndex:i]);
+    }
+}
 
 // 所有元素出栈，同时遍历
 -(void)traversalElementPopStack:(StackBlock)block {
-      // 先获取存储元素的个数
-      NSInteger count = self.stackArray.count;
-  for (NSInteger i = count; i > 0; i --) {
-          // 处理最后一个元素
-          block(self.stackArray.lastObject);
-          [self.stackArray removeLastObject];
-      }
-  }
+    // 先获取存储元素的个数
+    NSInteger count = self.stackArray.count;
+    for (NSInteger i = count; i > 0; i --) {
+        // 处理最后一个元素
+        block(self.stackArray.lastObject);
+        [self.stackArray removeLastObject];
+    }
+}
 
 // 返回栈顶元素
 -(id)topElemet {
-      return self.stackArray.lastObject;
-  }
+    return self.stackArray.lastObject;
+}
 
 // 清空
 -(void)removeAllObjects {
-     [self.stackArray removeAllObjects];
- }
+    [self.stackArray removeAllObjects];
+}
 
 #pragma mark - 懒加载
--(NSMutableArray*)stackArray{
+-(NSMutableArray*)stackArray {
     if (_stackArray == nil) {
-         _stackArray = [NSMutableArray array];
-     }
+        _stackArray = [NSMutableArray array];
+    }
     return _stackArray;
 }
--(NSInteger)top{
-     _top = self.stackArray.count;
-     return _top;
-  }
- 
- #pragma mark - 不存在该对象的时候，自动清空
- - (void)dealloc{
-     [self.stackArray removeAllObjects];
-     }
+-(NSInteger)top {
+    _top = self.stackArray.count;
+    return _top;
+}
+
+#pragma mark - 不存在该对象的时候，自动清空
+- (void)dealloc {
+    [self.stackArray removeAllObjects];
+}
 
 @end
