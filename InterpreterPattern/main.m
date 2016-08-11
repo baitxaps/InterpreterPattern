8fd62fb23ab79c9b1741d3035e3bcf01cb9411d3
rhc <bawfnhaps@163.com>
2016-08-11 18:15:09 +0800

    Initial Commit

---
+++ InterpreterPattern/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  InterpreterPattern
+//
+//  Created by hairong chen on 16/8/11.
+//  Copyright © 2016年 hairong chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
